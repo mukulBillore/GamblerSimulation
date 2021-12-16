de0ca6a55c9c11c47a94c28ffc3eae21ccf9c50d
mukulBillore <mukulbillore88@gmail.com>
2021-12-16 09:22:50 +0530

    Added/modified : uc2 is added to the code

--- com/bridgelabs/GamblerSimulator.java
+++ com/bridgelabs/GamblerSimulator.java
@@ -1,5 +1,7 @@
 package com.bridgelabs;
 
+import java.util.Random;
+
 public class GamblerSimulator {
 
 	public static void main(String[] args) {
@@ -8,6 +10,19 @@ public class GamblerSimulator {
 		int everyDayStake = 100;
 		int everyGameBet = 1;
 
+		//uc2 
+		Random rand = new Random();
+		int winOrLoose=rand.nextInt(2);
+		
+		if(winOrLoose==0) {
+			//win
+			everyDayStake=everyDayStake+1;
+		}
+		else {
+			everyDayStake=everyDayStake-1;
+		}
+		
+		
 	}
 
 }
