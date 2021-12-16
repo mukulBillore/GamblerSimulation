e82ce1d619c38f069714942a3f3b65adf8053014
mukulBillore <mukulbillore88@gmail.com>
2021-12-16 09:37:50 +0530

    Added/Modified uc4

--- com/bridgelabs/GamblerSimulator.java
+++ com/bridgelabs/GamblerSimulator.java
@@ -8,19 +8,22 @@ public class GamblerSimulator {
 
 		int everyDayStake = 100;
 		int everyGameBet = 1;
-//uc3
-		while (everyDayStake != 50 || everyDayStake != 150) {
-			Random rand = new Random();
-			int winOrLoose = rand.nextInt(2);
+		int totelStakeLeft = 0;
+		for (int i = 1; i <= 20; i++) {
+			while (everyDayStake != 50 || everyDayStake != 150) {
+				Random rand = new Random();
+				int winOrLoose = rand.nextInt(2);
 
-			if (winOrLoose == 0) {
-				// win
-				everyDayStake = everyDayStake + 1;
-			} else {// loose
-				everyDayStake = everyDayStake - 1;
+				if (winOrLoose == 0) {
+					// win
+					everyDayStake = everyDayStake + 1;
+				} else {// loose
+					everyDayStake = everyDayStake - 1;
+				}
+
 			}
-		}
-
+			totelStakeLeft = everyDayStake;
+		}System.out.println("The totel stake left after the completion of 20th day is : "+totelStakeLeft);
 	}
 
 }
